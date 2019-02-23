651132550b92b2f257a9728db46b5c2abf1737c4
mariomui <yi.kan.mui@gmail.com>
2019-02-22 17:36:12 -0800

    Added face module
    Also added started on the Home content page as a placeholder.

---
+++ src/components/HomeContent.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default () => {
+  return (
+    <Fragment>
+
+      <p style={{ color: 'green' }}>I've created inventory scanners using IOS devices and an API, saving time and money on software and hardware integration costs. </p>
+      <p> This website was built using Gatsby, BootswatchCDN and FontAwesome</p>
+    </Fragment>
+  )
+}
