72619fd82f624c24e5bbd5da890840e1bb3ece59
mariomui <yi.kan.mui@gmail.com>
2019-02-27 13:46:50 -0800

    Add button to layout to go to top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
       height: '10vh', width: '100%', backgroundColor: 'blue'
     }}
       className="navbar navbar-expand-lg navbar-dark bg-primary" > <div id="navbarColor01">
+        <a className="btn btn-danger btn-lgstyle" role="button" href="#top">Back to Top</a>
 
-        <a href="#top">Back to Top</a>
         <p style={{ color: 'white' }}> This website was built using Gatsby, BootswatchCDN and FontAwesome</p>
 
       </div >
-    </div>
+    </div >
   )
 }
 
